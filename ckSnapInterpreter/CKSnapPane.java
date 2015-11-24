abc5efc4217ea0589a5a25cda3dedbc4d38182aa
Michael K. Bradshaw <michael.bradshaw@centre.edu>
2015-11-24 16:38:09 -0500

    Added firebug to SNAP for debugging purposes.

--- ckSnapInterpreter/CKSnapPane.java
+++ ckSnapInterpreter/CKSnapPane.java
@@ -1,5 +1,11 @@
 package ckSnapInterpreter;
 
+import org.w3c.dom.Document;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.concurrent.Worker.State;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -33,8 +39,59 @@ public class CKSnapPane extends Pane {
 		CKGameObjectsFacade.setWebEngine(BrowserWindow.getEngine());
 		WebEngine webEngine = CKGameObjectsFacade.getWebEngine();
 		
+		
+		webEngine.setOnError(e->{System.err.println("JAVASCRIPT ERROR"+e.getMessage());});
+		webEngine.getLoadWorker().exceptionProperty()
+		.addListener((e,oldVal,newVal)->
+		{System.err.println("JAVASCRIPT LOAD ERROR"+e.toString()+oldVal.getMessage()+newVal.getMessage());});
+		/*webEngine.getLoadWorker().stateProperty().addListener(
+			      new ChangeListener<Worker.State>() {
+			      public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {                               
+			             System.out.println("webEngine result "+ newState.toString());
+			      }
+			  });
+		*/
+		 webEngine.getLoadWorker().stateProperty().addListener(
+		            new ChangeListener<State>() {
+		              @Override public void changed(ObservableValue ov, State oldState, State newState) {
+
+		                    System.out.println("called"+newState);
+		                  
+		                }
+		            });
+		 
+		 
+		 
+		 
+		 
+		//engine.executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}"); 
+
+		
+		/*webEngine.getLoadWorker().exceptionProperty().addListener(new ChangeListener<Throwable>() {
+		    @Override
+		    public void changed(ObservableValue<? extends Throwable> ov, Throwable t, Throwable t1) {
+		        System.out.println("Received exception: "+t1.getMessage());
+		    }
+		});
+		*/
+		
 		BrowserWindow.setPrefSize(690, 820);
 		webEngine.load(getClass().getResource("CyberSnap/snap.html").toExternalForm());
+		
+		
+		webEngine.documentProperty().addListener(new ChangeListener<Document>() {
+		      @Override public void changed(ObservableValue<? extends Document> prop, Document oldDoc, Document newDoc) {
+		    	webEngine.executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}"); 
+		      }
+		    });
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		JSObject jsobj = (JSObject) webEngine.executeScript("window");
 		jsobj.setMember("javaMove", new CKSpellObject("move"));
