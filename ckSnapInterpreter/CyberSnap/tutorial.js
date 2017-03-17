4cfaf6ef8b937523e77ce0a9f231fafd93a5260a
Nick Miller <Nick Miller>
2017-03-17 10:24:03 -0400

    Adding movement functions and step function to tutorial.js

--- ckSnapInterpreter/CyberSnap/tutorial.js
+++ ckSnapInterpreter/CyberSnap/tutorial.js
@@ -8,8 +8,9 @@ tutorial_Morph.prototype = new ShadowMorph();
 tutorial_Morph.prototype.constructor = tutorial_Morph;
 tutorial_Morph.uber = ShadowMorph.prototype;
 
-tutorial_Morph.prototype.init = function()
+tutorial_Morph.prototype.init = function(ide)
 {
+    this.ide = ide;
     this.setColor(new Color(238, 244, 66));
     this.alpha = 0;
     this.noticesTransparentClick = true;
@@ -18,8 +19,9 @@ tutorial_Morph.prototype.init = function()
     this.boxy.setLeft(10);
     this.boxy.setRight(100);
     this.boxy.setColor(new Color(100,100,100));
+    this.fps = 5;
+    console.log();
 }
-
 tutorial_Morph.prototype.openIn = function(world)
 {
     world.add(this);
@@ -57,9 +59,66 @@ tutorial_Morph.prototype.checkBlockState = function(ide,s,hat,test)
     }
     return found;
 }
-
-function tutorial_Morph()
+tutorial_Morph.prototype.moveMorph = function(clickmorph, movemorph)
 {
-    this.init();
+    //clickmorph: the morph on screen to click. Once clicked, the movemorph will move positions
+    //movemorph: the morph on screen to move. It will have an array of points as an attribute to move around the screen.
+      clickmorph.mouseClickLeft =  function()
+                    {
+                        var pos = 0;
+                        var point = movemorph.potentialPoints[movemorph.index];
+                        var deltax = point.x - movemorph.position().x;
+                        var deltay = point.y - movemorph.position().y;
+                        console.log(point);
+                        console.log("deltax: " + deltax + " deltay: " + deltay);
+                        id = setInterval(frame, 5);   
+                        function frame() {
+                                if (pos == 25) {
+                                    clearInterval(id);
+                                    movemorph.setPosition(point);
+                                    if ((movemorph.index+1) >= (movemorph.potentialPoints.length))
+                                        {
+                                            
+                                            movemorph.index = 0;
+                                        }
+                                    else{
+                                    
+                                    movemorph.index = movemorph.index + 1;
+                                    }
+                                } else {
+                                    pos = pos + 1;
+                                    movemorph.moveBy(new Point(deltax/25,deltay/25));
+                                }
+                    }      
+      }
+      
+}
+tutorial_Morph.prototype.setPoints = function(movemorph, points)
+{
+    //movemorph: morph to move across the screen.
+    //points: an array of points that the movemorph will move through
+    movemorph.potentialPoints = points;
+    movemorph.index = 0; //index for the next point to move to in array of potentialPoints
+}
+tutorial_Morph.prototype.step = function()
+{
+    if (this.checkBlockState(tutorial.ide, 0, 0, "forward"))
+        {
+            this.currentStateIndex = this.currentState.transition.nextState;
+            return this.finalState(this.currentStateIndex);
+        }
+    return false;
+}
+tutorial_Morph.prototype.finalState = function(index)
+{
+    if (this.currentStateIndex == this.finalStateIndex)
+        {
+            return true;
+        }
+    return false;
+}
+function tutorial_Morph(ide)
+{
+    this.init(ide);
 }
 
